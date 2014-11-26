459012051e9283cb58873d83562f227e83bb8553
Terry Chen <hahakuku@hotmail.com>
2014-11-26 03:17:53 -0500

    update

---
+++ Objectness/utils.h
@@ -0,0 +1,56 @@
+#pragma once
+
+
+const int ROW_DIM = 0;
+const int COL_DIM = 1;
+
+cv::Mat im2colstep(cv::Mat& InImg, vector<int>& blockSize, vector<int>& stepSize);
+
+cv::Mat im2col_general(cv::Mat& InImg, vector<int>& blockSize, vector<int>& stepSize);
+
+cv::Mat PCA_FilterBank(vector<cv::Mat>& InImg, int PatchSize, int NumFilters);
+
+typedef struct {
+	vector<cv::Mat> OutImg;
+	vector<int> OutImgIdx;
+} PCA_Out_Result;
+
+void PCA_output(PCA_Out_Result* &result,  vector<cv::Mat>& InImg, vector<int>& InImgIdx, int PatchSize, int NumFilters, cv::Mat& Filters, int threadnum);
+
+typedef struct {
+	//SparseMat Features;
+	cv::Mat Features;
+	vector<int> feature_idx;
+	vector<cv::Mat> Filters;
+	vector<int> BlkIdx;
+} PCA_Train_Result;
+
+typedef struct {
+	int NumStages;
+	int PatchSize;
+	vector<int> NumFilters;
+	vector<int> HistBlockSize;
+	double BlkOverLapRatio;
+} PCANet;
+
+PCA_Train_Result* PCANet_train(vector<cv::Mat>& InImg, PCANet* PcaNet, bool is_extract_feature);
+
+typedef struct {
+	//SparseMat Features;
+	cv::Mat Features;
+	vector<int> BlkIdx;
+} Hashing_Result;
+
+void HashingHist(Hashing_Result* &ha_result, PCANet* PcaNet, vector<int>& ImgIdx, vector<cv::Mat>& Imgs);
+
+cv::Mat Heaviside(cv::Mat& X);
+
+/**
+*	the range of the histogram is 0 to Range
+*/
+cv::Mat Hist(cv::Mat& mat, int Range);
+
+cv::Mat bsxfun_times(cv::Mat& BHist, int NumFilters);
+//SparseMat bsxfun_times(SparseMat BHist, int cols, int NumFilters);
+
+cv::Mat Mat_mul_MT(cv::Mat& mat);
---
+++ README.txt
@@ -0,0 +1,4 @@
+1. Objectness for potential bbox
+2. PCANet for feature extraction
+3. Liblinear SVM classifier
+4. Multi-threading support
